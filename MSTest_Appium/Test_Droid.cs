8db7b238f56ea76427c688d4752b6f9f8d8f3544
Michael <Michael@ZPF.fr>
2026-03-16 09:52:56 +0100

    Update Test_Droid.cs

--- MSTest_Appium/Test_Droid.cs
+++ MSTest_Appium/Test_Droid.cs
@@ -13,14 +13,17 @@ namespace MauiApp.UITests
       [TestInitialize]
       public void Setup()
       {
+         // C:\Program Files (x86)\Android\android-sdk\emulator\emulator -avd pixel_7_-_api_36_0
+
+
          var options = new AppiumOptions();
          options.PlatformName = "Android";
-         options.DeviceName = "emulator-5554";
+         options.DeviceName = "pixel_7_-_api_36_0";
          options.AutomationName = "UiAutomator2";
 
          // Path to your MAUI .apk
-         options.App = @"C:\Users\zepro\AppData\Local\Xamarin\Mono for Android\Archives\2026-03-14\Maui 3-14-26 5.21 AM.apkarchive\com.companyname.maui.apk";
-
+         // options.App = @"C:\Users\zepro\AppData\Local\Xamarin\Mono for Android\Archives\2026-03-14\Maui 3-14-26 5.21 AM.apkarchive\com.companyname.maui.apk";
+         options.App = @"C:\Users\zepro\AppData\Local\Xamarin\Mono for Android\Archives\2026-03-16\Maui 3-16-26 9.30 AM.apkarchive\com.companyname.maui.apk";
          _driver = new AndroidDriver(new Uri("http://127.0.0.1:4723"), options);
          _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
       }
@@ -36,10 +39,10 @@ namespace MauiApp.UITests
       {
          // Find elements by MAUI AutomationId
          var button = _driver.FindElement(MobileBy.AccessibilityId("CounterBtn"));
-         var label = _driver.FindElement(MobileBy.AccessibilityId("CounterLabel"));
+         //var label = _driver.FindElement(MobileBy.AccessibilityId("CounterLabel"));
 
          // Initial state
-         Assert.AreEqual("Current count: 0", label.Text);
+         // Assert.AreEqual("Current count: 0", label.Text);
 
          // Click button
          button.Click();
